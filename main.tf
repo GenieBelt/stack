feca4ccdfa322180c6f4c0f46a3fb7d4ff494d9a
aligb <ali@geniebelt.com>
2017-03-13 00:27:00 +0100

    internal security group added

--- main.tf
+++ main.tf
@@ -324,5 +324,7 @@ output "internal_security_groups" {
   value = ["${module.security_groups.internal_elb}",
            "${module.security_groups.external_elb}",
            "${module.security_groups.internal_ssh}",
-           "${module.security_groups.external_ssh}" ]
+           "${module.security_groups.external_ssh}",
+           "${module.ecs_cluster.security_group_id}"]
+}]
 }
