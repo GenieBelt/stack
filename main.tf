dde89cdb3140e8a2dbcd9598839ee584be412b3a
Martin Koch <martin@geniebelt.com>
2017-06-20 15:39:06 +0200

    Added redis acces to cluster security group

--- main.tf
+++ main.tf
@@ -213,7 +213,7 @@ module "ecs_cluster" {
   docker_volume_size     = "${var.ecs_docker_volume_size}"
   docker_auth_type       = "${var.ecs_docker_auth_type}"
   docker_auth_data       = "${var.ecs_docker_auth_data}"
-  security_groups        = "${coalesce(var.ecs_security_groups, format("%s,%s,%s", module.security_groups.internal_ssh, module.security_groups.internal_elb, module.security_groups.external_elb))}"
+  security_groups        = "${coalesce(var.ecs_security_groups, format("%s,%s,%s", module.security_groups.internal_ssh, module.security_groups.internal_elb, module.security_groups.external_elb, module.security_groups.redis_access))}"
   extra_cloud_config_type     = "${var.extra_cloud_config_type}"
   extra_cloud_config_content  = "${var.extra_cloud_config_content}"
 }
